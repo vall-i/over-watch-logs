c926c0045d6ede305f698ca6b9e0579e93e40a79
vall-i <valik-way@rambler.ru>
2021-04-29 19:32:38 +0300

    Updates

---
+++ precache-manifest.ead4fb1f26ae4719399e823d07342a47.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa6c7e03fcbdb0784ad5e0ef4480cb1",
+    "url": "/over-watch-logs/index.html"
+  },
+  {
+    "revision": "8bd3883344400c3cde47",
+    "url": "/over-watch-logs/static/css/2.edb8eddf.chunk.css"
+  },
+  {
+    "revision": "b943873bbcf910d5d426",
+    "url": "/over-watch-logs/static/css/4.1d5bcbcf.chunk.css"
+  },
+  {
+    "revision": "341edcb31672022ff4fb",
+    "url": "/over-watch-logs/static/css/main.1cd00525.chunk.css"
+  },
+  {
+    "revision": "8bd3883344400c3cde47",
+    "url": "/over-watch-logs/static/js/2.0b36b544.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/over-watch-logs/static/js/2.0b36b544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06fe01bd90c3593fe5f6",
+    "url": "/over-watch-logs/static/js/3.3cd8eceb.chunk.js"
+  },
+  {
+    "revision": "b943873bbcf910d5d426",
+    "url": "/over-watch-logs/static/js/4.4b5f2c3b.chunk.js"
+  },
+  {
+    "revision": "341edcb31672022ff4fb",
+    "url": "/over-watch-logs/static/js/main.01088a28.chunk.js"
+  },
+  {
+    "revision": "e2b501b70cd2640f0ddc",
+    "url": "/over-watch-logs/static/js/runtime-main.4608afe6.js"
+  },
+  {
+    "revision": "77f3995e0e869a78fd7a38f6c4493484",
+    "url": "/over-watch-logs/static/media/logo.77f3995e.png"
+  }
+]);
